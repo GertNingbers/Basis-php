58a383f289e6a1860d9a4ccbe542c22f15b6204f
GertNingbers <97081115@st.deltion.nl>
2024-11-27 12:06:14 +0100

    V1 simpel login

---
+++ simpel login/simpel-login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="simplelogin.css">
+    <title>simple-login</title>
+</head>
+
+<body>
+    <h1>Login WEB</h1>
+ 
+    <form method="post">
+        Naam:
+        <input type="text" name="naam" placeholder="naam" required><br>
+        wachtwoord:
+        <input type="password" name="wachtwoord" placeholder="wachtwoord" required><br>
+        <input type="submit" name="verzend">
+        </form>
+
+        <?php
+    $account = array('admin', 'kip', 'meneer', 'koelkast', 'stoofpot');
+    $wachtwoord = array(
+        sha1('1234'),
+        sha1('5555'),
+        sha1('9999'),
+        sha1('1111'),
+        sha1('2222')
+    );
+
+    if (isset($_POST['naam']) && isset($_POST['wachtwoord'])) {
+        $index = array_search($_POST['naam'], $account);
+        $hashedpassword = sha1($_POST['wachtwoord']);
+
+        if ($index !== false && $wachtwoord[$index] === $hashedpassword) {
+            header('Location: login-goed.php');
+            exit(); 
+        } else {
+            echo "Uw naam of wachtwoord is incorrect!";
+        }
+    }
+    ?>
+</body>
+
+</html>
