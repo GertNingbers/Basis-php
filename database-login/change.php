9e697ea9a548314726dda3aedc38bee5f1a61b1f
GertNingbers <97081115@st.deltion.nl>
2024-12-09 09:23:12 +0100

    klaar database login1

--- database-login/change.php
+++ database-login/change.php
@@ -31,11 +31,11 @@ if (!isset($_SESSION['user_id'])) {
             <h2>Update Account</h2>
 
             <form action="includes/userupdate.inc.php" method="post">
-                <p>Firstname: <input type="text" name="firstname" placeholder="Firstname" value="<?php echo htmlspecialchars($_SESSION['firstname']); ?>"></p>
-                <p>Lastname: <input type="text" name="lastname" placeholder="Lastname" value="<?php echo htmlspecialchars($_SESSION['lastname']); ?>"></p>
-                <p>Email: <input type="text" name="email" placeholder="Email" value="<?php echo htmlspecialchars($_SESSION['email']); ?>"></p>
-                <p>Username: <input type="text" name="account_name" placeholder="Account_name" value="<?php echo htmlspecialchars($_SESSION['username']); ?>"></p>
-                <p>Password: <input type="password" name="pwd" placeholder="Password" value="<?php echo htmlspecialchars($_SESSION['pwd']); ?>"></p>
+                <p>Firstname: <input type="text" name="firstname" placeholder="Firstname" value="<?php echo htmlspecialchars($_SESSION['firstname']); ?>" required></p>
+                <p>Lastname: <input type="text" name="lastname" placeholder="Lastname" value="<?php echo htmlspecialchars($_SESSION['lastname']); ?>" required></p>
+                <p>Email: <input type="text" name="email" placeholder="Email" value="<?php echo htmlspecialchars($_SESSION['email']); ?>" required></p>
+                <p>Username: <input type="text" name="account_name" placeholder="Account_name" value="<?php echo htmlspecialchars($_SESSION['username']); ?>" required></p>
+                <p>Password: <input type="password" name="pwd" placeholder="Password" required></p>
                 <input type="submit" name="update" value="Update" class="btn">
             </form>
         </div>
--- database-login/includes/loginform.inc.php
+++ database-login/includes/loginform.inc.php
@@ -5,7 +5,6 @@ session_start();
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
-    $_SESSION['pwd'] = $password;
 
     try {
         require_once "dbh.inc.php";
